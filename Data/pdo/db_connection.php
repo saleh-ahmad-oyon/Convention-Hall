d4e88341a111386d14a16137702e5f1aee6aedb1
Saleh Ahmad <salehoyon@hotmail.com>
2016-02-18 01:19:58 +0600

    add pdo in Data

---
+++ Data/pdo/db_connection.php
@@ -0,0 +1,16 @@
+<?php
+    function db_conn(){
+        $SERVER = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $db = 'convention_hall';
+        try{
+            $conn = new PDO('mysql:host='.$SERVER.';dbname='.$db.';charset=utf8', $user, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        }catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $conn;
+    }
+?>
