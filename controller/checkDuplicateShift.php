39ba7bcc4a430f809b6fe8f1c0c7cf4859ba3e2d
Saleh Ahmad <salehoyon@hotmail.com>
2016-08-30 06:21:19 +0600

    v 1.0

--- controller/checkDuplicateShift.php
+++ controller/checkDuplicateShift.php
@@ -1,10 +1,11 @@
 <?php
-    require '../model/db.php';
-    $value = $_GET['query'];
-    $dob = $_GET['date'];
-    $date = str_replace('/', '-', $dob);
-    $date = date('Y-m-d', strtotime($date));
+require '../model/db.php';
 
-    if(similarDateShift($date, $value)){
-        echo "**$value has already booked !! Please select another shift.";
-    }
+$value = $_GET['query'];
+$dob   = $_GET['date'];
+$date  = str_replace('/', '-', $dob);
+$date  = date('Y-m-d', strtotime($date));
+
+if (similarDateShift($date, $value)) {
+    echo "**$value has already booked !! Please select another shift.";
+}
