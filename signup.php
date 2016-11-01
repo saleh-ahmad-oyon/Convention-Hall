8583b8f9a305c24e09ea642401f722d897d4fbaa
Saleh Ahmad <salehoyon@hotmail.com>
2016-11-02 01:42:34 +0600

    v 1.0

--- signup.php
+++ signup.php
@@ -39,32 +39,48 @@
                                     <form action="<?php echo SERVER; ?>/controller/signUpSuccess" method="post" onsubmit="return confirmation();">
                                 <div class="form-group">
                                     <label>First Name</label>
-                                    <input type="text" class="onlyChars form-control" autofocus="autofocus" name="fname" pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}" title="Only letters, space, dot and comma allowed" />
+                                    <input type="text" class="onlyChars form-control" autofocus="autofocus" name="fname"
+                                           pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}" title="Only letters, space, dot and comma allowed" />
                                 </div>
                                 <div class="form-group">
                                     <label>Last Name</label>
-                                    <input type="text" class="onlyChars form-control" name="lname" pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}" title="Only letters, space, dot and comma allowed" />
+                                    <input type="text" class="onlyChars form-control" name="lname"
+                                           pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}" title="Only letters, space, dot and comma allowed" />
                                 </div>
                                 <div class="form-group" id="emailVal">
                                     <p id="email" class="text-danger"></p>
                                     <label>Email</label>
-                                    <input type="email" class="form-control has-success has-feedback" id='email1' name="email" required="required" pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Insert Email Correctly' : ''); if(this.checkValidity()) form.cemail.pattern = this.value;" onblur="validate('email', this.value)" />
+                                    <input type="email"
+                                           class="form-control has-success has-feedback"
+                                           id='email1'
+                                           name="email"
+                                           required="required"
+                                           pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i"
+                                           onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Insert Email Correctly' : ''); if(this.checkValidity()) form.cemail.pattern = this.value;"
+                                           onblur="validate('email', this.value)" />
                                 </div>
                                 <div class="form-group">
                                     <label>Confirm Email</label>
-                                    <input type="email" class="form-control" name="cemail" required="required" pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same email as above' : '');" />
+                                    <input type="email" class="form-control" name="cemail" required="required"
+                                           pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i"
+                                           onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same email as above' : '');" />
                                 </div>
                                 <div class="form-group">
                                     <label>Contact No.</label>
-                                    <input type="tel" class="phnNum form-control" name="contact" id="phone" required="required" pattern="(?=.*[0-9])+.{5,}" title="please insert a valid Phone Number" />
+                                    <input type="tel" class="phnNum form-control" name="contact" id="phone" required="required"
+                                           pattern="(?=.*[0-9])+.{5,}" title="please insert a valid Phone Number" />
                                 </div>
                                 <div class="form-group">
                                     <label>Password</label>
-                                    <input type="password" class="form-control" name="pass" required="required" id="newPass" pattern="(?=^.{4,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'The password must contain one or more uppercase characters, one or more lowercase characters, one or more numeric values, one or more special characters, and length must be greater than or equal to 4' : ''); if(this.checkValidity()) form.cpass.pattern = this.value;" />
+                                    <input type="password" class="form-control" name="pass" required="required" id="newPass"
+                                           pattern="(?=^.{4,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                                           onchange="this.setCustomValidity(this.validity.patternMismatch ? 'The password must contain one or more uppercase characters, one or more lowercase characters, one or more numeric values, one or more special characters, and length must be greater than or equal to 4' : ''); if(this.checkValidity()) form.cpass.pattern = this.value;" />
                                 </div>
                                 <div class="form-group">
                                     <label>Confirm Password</label>
-                                    <input type="password" class="form-control" name="cpass" required="required" id="confirmNewPass" pattern="(?=^.{4,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');" />
+                                    <input type="password" class="form-control" name="cpass" required="required" id="confirmNewPass"
+                                           pattern="(?=^.{4,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                                           onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');" />
                                 </div>
                                 <div class="form-group">
                                     <div class="cf">
@@ -93,6 +109,7 @@
         <footer>
             <?php require "includes/footer.php";?>
         </footer>
+
         <script src="<?= SERVER; ?>/assets/js/jquery-2.2.0.min.js"></script>
         <script src="<?= SERVER; ?>/assets/js/checkbox.js"></script>
         <script src="<?= SERVER; ?>/assets/js/jquery.maskedinput.min.js"></script>
