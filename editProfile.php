26b4184cb191f6de72908b1413cc8b7bf4a7d6cd
Saleh Ahmad <salehoyon@hotmail.com>
2016-11-02 22:15:08 +0600

    v 1.0

--- editProfile.php
+++ editProfile.php
@@ -1,31 +1,29 @@
 <?php
-session_start();
-require 'controller/define.php';
-require_once 'controller/indexControl.php';
-$login =false;
-if(isset($_COOKIE['user']) || isset($_SESSION['user'])){
-    if(!isset($_SESSION['user'])){
-        $_SESSION['user'] = $_COOKIE['user'];
-    }
-    $login = true;
+    session_start();
 
-    $user = $_SESSION['user'];
-    $proInfo = getPersonalInfo($user);
-} elseif(!$login){
-    header('Location: '.SERVER.'/404');
-}
+    require 'controller/define.php';
+    require_once 'controller/indexControl.php';
+
+    $login = false;
+    if (isset($_COOKIE['user']) || isset($_SESSION['user'])) {
+        if (!isset($_SESSION['user'])) {
+            $_SESSION['user'] = $_COOKIE['user'];
+        }
+        $login = true;
+
+        $user    = $_SESSION['user'];
+        $proInfo = getPersonalInfo($user);
+    } elseif (!$login) {
+        header('Location: '.SERVER.'/404');
+    }
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
     <?php require_once 'includes/head.php'; ?>
-    <link href="<?php echo SERVER; ?>/assets/css/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="<?php echo SERVER; ?>/assets/css/custom.css" rel="stylesheet"/>
-    <link href="<?php echo SERVER; ?>/assets/css/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet"/>
-    <script src="<?php echo SERVER; ?>/assets/js/jquery-2.2.0.min.js"></script>
-    <script src="<?php echo SERVER; ?>/assets/css/bootstrap-3.3.5-dist/js/bootstrap.js"></script>
-    <script src="<?php echo SERVER; ?>/assets/js/jquery.maskedinput.min.js"></script>
-    <script src="<?php echo SERVER; ?>/assets/js/custom.js"></script>
+    <link href="<?= SERVER; ?>/assets/css/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="<?= SERVER; ?>/assets/css/custom.css" rel="stylesheet"/>
+    <link href="<?= SERVER; ?>/assets/css/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet"/>
     <style>
         body { padding-top: 51px; }
     </style>
@@ -48,19 +46,37 @@ if(isset($_COOKIE['user']) || isset($_SESSION['user'])){
                                     <form action="<?php echo SERVER; ?>/controller/editInfoSuccess.php" method="post">
                                         <div class="form-group">
                                             <label>Name</label>
-                                            <input type="text" class="onlyChars form-control" name="userName" value="<?php echo htmlentities(stripslashes($proInfo['u_fname'])), " ", htmlentities(stripslashes($proInfo['u_lname'])); ?>" pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}" title="Only letters, space, dot and comma allowed"/>
+                                            <input type="text"
+                                                   class="onlyChars form-control"
+                                                   name="userName"
+                                                   value="<?= htmlentities(stripslashes($proInfo['u_fname'])), " ", htmlentities(stripslashes($proInfo['u_lname'])); ?>"
+                                                   pattern="(?=.*[a-zA-Z])+([.,\s])*.{2,}"
+                                                   title="Only letters, space, dot and comma allowed"/>
                                         </div>
                                         <div class="form-group">
                                             <label>Email</label>
-                                            <input type="email" class="form-control" name="email" value="<?php echo htmlentities(stripslashes($proInfo['u_email'])); ?>" pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i" title="Please insert a valid email" />
+                                            <input type="email"
+                                                   class="form-control"
+                                                   name="email"
+                                                   value="<?= htmlentities(stripslashes($proInfo['u_email'])); ?>"
+                                                   pattern="[([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)]i"
+                                                   title="Please insert a valid email" />
                                         </div>
                                         <div class="form-group">
                                             <label>Contact</label>
-                                            <input type="tel" class="phnNum form-control" name="contact" id="phone" value="<?php echo htmlentities(stripslashes($proInfo['u_contact'])); ?>" />
+                                            <input type="tel"
+                                                   class="phnNum form-control"
+                                                   name="contact"
+                                                   id="phone"
+                                                   value="<?= htmlentities(stripslashes($proInfo['u_contact'])); ?>" />
                                         </div>
                                         <br/>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-success" name="editInfo" pattern="(?=.*[0-9])+.{5,}" title="please insert a valid Phone Number">Update</button>
+                                            <button type="submit"
+                                                    class="btn btn-success"
+                                                    name="editInfo"
+                                                    pattern="(?=.*[0-9])+.{5,}"
+                                                    title="please insert a valid Phone Number">Update</button>
                                         </div>
                                     </form>
                                 </div>
@@ -76,5 +92,10 @@ if(isset($_COOKIE['user']) || isset($_SESSION['user'])){
     <footer>
         <?php require "includes/footer.php";?>
     </footer>
+
+    <script src="<?= SERVER; ?>/assets/js/jquery-2.2.0.min.js"></script>
+    <script src="<?= SERVER; ?>/assets/css/bootstrap-3.3.5-dist/js/bootstrap.js"></script>
+    <script src="<?= SERVER; ?>/assets/js/jquery.maskedinput.min.js"></script>
+    <script src="<?= SERVER; ?>/assets/js/custom.js"></script>
 </body>
 </html>
