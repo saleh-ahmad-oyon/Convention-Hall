2e7f2bb997bae0576f5488e9bbf8cf9d82c5212e
Saleh Ahmad <salehoyon@hotmail.com>
2016-11-15 21:47:13 +0600

    v 1.0

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php require 'controller/define.php'; ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php require_once 'includes/head.php'; ?>
-        <link href="<?= SERVER; ?>/assets/css/custom.css" rel="stylesheet"/>
-    </head>
-    <body>
-        <div class="cover-image-404"></div>
-    </body>
+<head>
+  <?php require_once 'includes/head.php'; ?>
+  <link href="<?= SERVER; ?>/assets/css/custom.css" rel="stylesheet"/>
+</head>
+<body>
+  <div class="cover-image-404"></div>
+</body>
 </html>
