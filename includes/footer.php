6c2b3e5ce24e8055f6255fe080c83b949dddbfa8
Saleh Ahmad <salehoyon@hotmail.com>
2016-11-16 02:18:16 +0600

    v 1.0

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,17 @@
 <div class="container">
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <span>Designed by <strong>Saleh Ahmad</strong></span><br/>
-        <cite>Convention Hall Booking</cite> &copy; <?php echo date("Y") ?>
+  <div class="col-sm-4 padding-top-20">
+    <span>Designed by <strong>Saleh Ahmad</strong></span><br/>
+    <cite>Convention Hall Booking</cite> &copy; <?php echo date("Y") ?>
+  </div>
+  <div class="col-sm-4 padding-top-20">
+    <span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;<a href="mailto:salehoyon@hotmail.com?cc=nissongo102@gmail.com&amp;subject=Feedback&amp;body=Dear%20Buddy%2C%0A" target="_top">salehoyon@hotmail.com</a><br/>
+    <span class="glyphicon glyphicon-earphone"></span>&nbsp;&nbsp;+880-1626785569<br/>
+    <span class="glyphicon glyphicon-map-marker"></span>&nbsp;&nbsp;10/181, 12/B, Mirpur, Dhaka-1216
+  </div>
+  <div class="col-sm-4 padding-top-20">
+    <div class="col-sm-6">
+      <a href="<?= SERVER ?>/hall" target="_blank">Hall Gallery</a><br/>
+      <a href="<?= SERVER ?>/location" target="_blank">Our Location</a>
     </div>
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;<a href="mailto:salehoyon@hotmail.com?cc=nissongo102@gmail.com&amp;subject=Feedback&amp;body=Dear%20Buddy%2C%0A" target="_top">salehoyon@hotmail.com</a><br/>
-        <span class="glyphicon glyphicon-earphone"></span>&nbsp;&nbsp;+880-1626785569<br/>
-        <span class="glyphicon glyphicon-map-marker"></span>&nbsp;&nbsp;10/181, 12/B, Mirpur, Dhaka-1216
-    </div>
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <div class="col-xs-4 col-sm-12 col-md-12 col-lg-12">
-            <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-                <a href="<?php echo SERVER ?>/hall" target="_blank">Hall View</a><br/>
-                <a href="<?php echo SERVER ?>/location" target="_blank">Our Location</a>
-            </div>
-            <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-
-            </div>
-        </div>
-    </div>
+  </div>
 </div>
